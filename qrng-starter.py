54f4efdfb26ba41a1067ac25cb3c6286aceef385
brokoli <bocalist@gmail.com>
2025-01-30 13:31:56 -0700

    changed comment in english

--- qrng-starter.py
+++ qrng-starter.py
@@ -1,19 +1,22 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
 
-number_of_qbits = 4  # 큐비트 수
+number_of_qbits = 4  # number of quantum bits
 qc = QuantumCircuit(number_of_qbits)
 
-# Hadamard 게이트로 중첩 상태 생성
+# H gate implementation
 for i in range(number_of_qbits):
     qc.h(i)
 
-qc.measure_all()  # 측정
+# Measure all qbits
+qc.measure_all()
 
+# Run the quantum circuit on a simulator backend
 simulator = AerSimulator()
 compiled_circuit = transpile(qc, simulator)
 result = simulator.run(compiled_circuit).result()
 
+# Get the counts of the result
 counts = result.get_counts()
 quantum_bin = list(counts.keys())[0]  # 랜덤 비트 결과
 print("Quantum RNG Binary Result:", quantum_bin)
