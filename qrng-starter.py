a6416b6005a53fadaaf98df56e91e8d230e39a3c
brokoli <bocalist@gmail.com>
2025-01-30 13:35:05 -0700

    added more quantum circuit that introduces more complexity

--- qrng-starter.py
+++ qrng-starter.py
@@ -4,9 +4,18 @@ from qiskit_aer import AerSimulator
 number_of_qbits = 4  # number of quantum bits
 qc = QuantumCircuit(number_of_qbits)
 
-# H gate implementation
+# Hadamard gates to create superposition
 for i in range(number_of_qbits):
     qc.h(i)
+
+# Entanglement using CNOT gates
+qc.cx(0, 1)
+qc.cx(1, 2)
+qc.cx(2, 3)
+
+# Adding phase shifts for more randomness
+qc.s(0)  # S gate (π/2 phase shift)
+qc.t(2)  # T gate (π/4 phase shift)
 
 # Measure all qbits
 qc.measure_all()
@@ -18,5 +27,9 @@ result = simulator.run(compiled_circuit).result()
 
 # Get the counts of the result
 counts = result.get_counts()
-quantum_bin = list(counts.keys())[0]  # 랜덤 비트 결과
+
+# Get the binary result
+quantum_bin = list(counts.keys())[0]
+
+# Print the result
 print("Quantum RNG Binary Result:", quantum_bin)
